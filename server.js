3c33a7f8fe37a83debf77c1d8b0b88c2d786e464
Chris Chitla <chrischitla@gmail.com>
2020-02-12 19:47:51 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+
+
+
+
+// Send every other request to the React app (API routes before this!)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+// for socket.io; let server = app.listen..... then socket(server)
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
